fff95758c8da4ca65f55d82bda40bf1ef593b5cb
Paloma Reynolds <palomap.paula@hotmail.com>
2024-05-06 17:32:11 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const body = document.querySelector('body');
+    const title = document.createElement('p');
+    title.innerText = 'Hello'
+    body.appendChild(title)
+
+    
+})
